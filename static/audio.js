13261f64c3cbe54a6e98c66d0a3aa65fa5fc00a8
Nerixyz <nerixdev@outlook.de>
2022-10-14 18:07:20 +0200

    feat: homepage

--- static/audio.js
+++ static/audio.js
@@ -1,32 +1,32 @@
 (function main() {
-    const audioEl = document.getElementById('file');
-    const playbackSpeedEl = document.getElementById('playback-speed');
-    const playbackSpeedLbl = document.getElementById('playback-speed-lbl');
-    const resetPlaybackSpeedEl = document.getElementById('reset-playback-speed');
-    const preservePitchEl = document.getElementById('preserve-pitch');
+  const audioEl = document.getElementById('file');
+  const playbackSpeedEl = document.getElementById('playback-speed');
+  const playbackSpeedLbl = document.getElementById('playback-speed-lbl');
+  const resetPlaybackSpeedEl = document.getElementById('reset-playback-speed');
+  const preservePitchEl = document.getElementById('preserve-pitch');
 
-    const updatePlaybackSpeed = () => {
-        audioEl.playbackRate = playbackSpeedEl.valueAsNumber;
-        playbackSpeedLbl.textContent = `${playbackSpeedEl.valueAsNumber.toFixed(1)}x`;
-    };
-    const updatePreservePitch = () => {
-        audioEl.preservesPitch = preservePitchEl.checked;
-        audioEl.mozPreservesPitch = audioEl.preservesPitch;
-    };
-    playbackSpeedEl.addEventListener('input', updatePlaybackSpeed);
-    resetPlaybackSpeedEl.addEventListener('click', () => {
-        playbackSpeedEl.value = '1';
-        updatePlaybackSpeed();
-    });
-    preservePitchEl.addEventListener('input', updatePreservePitch);
-
-    // the browser might have set some defualt values
+  const updatePlaybackSpeed = () => {
+    audioEl.playbackRate = playbackSpeedEl.valueAsNumber;
+    playbackSpeedLbl.textContent = `${playbackSpeedEl.valueAsNumber.toFixed(1)}x`;
+  };
+  const updatePreservePitch = () => {
+    audioEl.preservesPitch = preservePitchEl.checked;
+    audioEl.mozPreservesPitch = audioEl.preservesPitch;
+  };
+  playbackSpeedEl.addEventListener('input', updatePlaybackSpeed);
+  resetPlaybackSpeedEl.addEventListener('click', () => {
+    playbackSpeedEl.value = '1';
     updatePlaybackSpeed();
-    updatePreservePitch();
+  });
+  preservePitchEl.addEventListener('input', updatePreservePitch);
 
-    audioEl.addEventListener('error', () => {
-        if(audioEl.error && audioEl.error.code === 4) {
-            location.href = audioEl.src;
-        }
-    });
+  // the browser might have set some defualt values
+  updatePlaybackSpeed();
+  updatePreservePitch();
+
+  audioEl.addEventListener('error', () => {
+    if (audioEl.error && audioEl.error.code === 4) {
+      location.href = audioEl.src;
+    }
+  });
 })();
--- static/home.js
+++ static/home.js
@@ -1,3 +1,316 @@
-(function main() {
+const LOCAL_STORAGE_KEY = 'uploader-authorization';
 
+(function main() {
+  makeAuthStuff();
+  makeUploadStuff();
 })();
+
+function makeAuthStuff() {
+  const view = document.getElementById('view');
+  const saveForm = document.getElementById('set-auth-form');
+  const authInput = document.getElementById('auth-input');
+  const clearAuth = document.getElementById('clear-auth');
+
+  const updateState = () => {
+    if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
+      view.classList.add('authenticated');
+    } else {
+      view.classList.remove('authenticated');
+    }
+  };
+  const setToken = tok => {
+    if (tok) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, tok);
+    } else {
+      localStorage.removeItem(LOCAL_STORAGE_KEY);
+    }
+    updateState();
+  };
+
+  saveForm.addEventListener('submit', e => {
+    e.preventDefault();
+    setToken(authInput.value);
+  });
+  clearAuth.addEventListener('click', e => {
+    e.preventDefault();
+    setToken(null);
+  });
+  updateState();
+}
+
+function makeUploadStuff() {
+  const uploadForm = document.getElementById('upload-form');
+  const fileInput = document.getElementById('upload-file');
+  const fileLabelWrap = document.getElementById('file-label');
+  const fileText = document.getElementById('filename');
+  const noFilenameText = fileText.textContent;
+
+  let currentFile = null;
+  let dragRc = 0;
+  let dndDialog = null;
+  const clearDialog = () => {
+    if (dndDialog) {
+      console.trace('xd');
+      dndDialog.remove();
+      dndDialog = null;
+    }
+  };
+
+  uploadForm.addEventListener('submit', e => {
+    e.preventDefault();
+    if (currentFile) {
+      uploadFile(currentFile).catch(console.error);
+    }
+  });
+  const updateFilename = (fromInput = false) => {
+    if (fromInput && fileInput.files?.length) {
+      currentFile = fileInput.files[0];
+    }
+
+    if (currentFile) {
+      fileText.textContent = currentFile.name;
+      fileLabelWrap.classList.add('has-file');
+    } else {
+      fileText.textContent = noFilenameText;
+      fileLabelWrap.classList.remove('has-file');
+    }
+  };
+  fileInput.addEventListener('input', () => updateFilename(true));
+  updateFilename(true);
+
+  document.body.addEventListener('dragenter', e => {
+    e.stopPropagation();
+    e.preventDefault();
+    dragRc++;
+    if (!dndDialog) {
+      dndDialog = createOverlay({ title: 'Drop here!' });
+    }
+  });
+  document.body.addEventListener('dragover', e => {
+    e.stopPropagation();
+    e.preventDefault();
+  });
+  document.body.addEventListener('drop', e => {
+    e.stopPropagation();
+    e.preventDefault();
+    clearDialog();
+
+    if (e.dataTransfer?.files?.length) {
+      currentFile = e.dataTransfer.files[0];
+      updateFilename(false);
+    }
+  });
+  document.body.addEventListener('dragleave', () => {
+    dragRc--;
+    if (dragRc <= 0) {
+      clearDialog();
+    }
+  });
+  document.body.addEventListener('dragend', clearDialog);
+}
+
+function createOverlay({ title, content }) {
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+  document.body.append(overlay);
+
+  const view = document.createElement('div');
+  view.classList.add('overlay-view');
+  overlay.append(view);
+
+  const titleEl = document.createElement('h1');
+  titleEl.textContent = title;
+  view.append(titleEl);
+
+  function appendContent(onto, content) {
+    if (!content) {
+      content = document.createElement('div');
+    }
+    content.classList.add('overlay-content');
+    onto.append(content);
+
+    return content;
+  }
+
+  content = appendContent(view, content);
+
+  return {
+    overlay,
+    view,
+    titleEl,
+    content,
+    update({ title, content }) {
+      this.titleEl.textContent = title;
+      this.content.remove();
+      this.content = appendContent(this.view, content);
+    },
+    remove() {
+      const anim = this.overlay.animate(
+        {
+          opacity: ['1', '0'],
+          'backdrop-filter': ['blur(20px)', 'blur(0)'],
+        },
+        { duration: 200 },
+      );
+      anim.addEventListener('finish', () => this.overlay.remove());
+      anim.play();
+    },
+  };
+}
+
+function createSuccess(link, cb) {
+  const wrap = document.createElement('div');
+  wrap.classList.add('success');
+
+  const linkInfoEl = document.createElement('div');
+  linkInfoEl.classList.add('link-info');
+
+  const linkEl = document.createElement('a');
+  linkEl.href = link;
+  linkEl.textContent = link;
+  linkEl.target = '_blank';
+  linkInfoEl.append(linkEl);
+
+  const copyButton = document.createElement('button');
+  copyButton.classList.add('icon-button');
+
+  copyButton.textContent = 'Copy';
+
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('viewBox', '0 0 24 24');
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttribute(
+    'd',
+    'M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z',
+  );
+  path.setAttribute('fill', 'currentColor');
+  svg.append(path);
+  copyButton.append(svg);
+
+  copyButton.addEventListener('click', () => {
+    navigator.clipboard
+      .writeText(link)
+      .then(() =>
+        copyButton.animate(
+          {
+            transform: ['scale(1)', 'scale(1.1)', 'scale(1)'],
+          },
+          { duration: 150 },
+        ),
+      )
+      .catch(console.error);
+  });
+
+  linkInfoEl.append(copyButton);
+
+  wrap.append(linkInfoEl);
+
+  const exit = document.createElement('button');
+  exit.textContent = 'Ok';
+  exit.classList.add('icon-button');
+  exit.addEventListener('click', cb);
+  requestAnimationFrame(() => copyButton.focus());
+  wrap.append(exit);
+
+  return wrap;
+}
+
+function createError(text, cb) {
+  const wrap = document.createElement('div');
+  wrap.classList.add('error');
+
+  const textEl = document.createElement('h4');
+  textEl.textContent = text;
+  wrap.append(textEl);
+
+  const exit = document.createElement('button');
+  exit.textContent = 'Ok';
+  exit.classList.add('icon-button');
+  exit.addEventListener('click', cb);
+  requestAnimationFrame(() => exit.focus());
+  wrap.append(exit);
+
+  return wrap;
+}
+
+function createUploading() {
+  const wrap = document.createElement('div');
+  wrap.classList.add('uploading');
+
+  const progress = document.createElement('div');
+  progress.classList.add('loader');
+  wrap.append(progress);
+
+  const text = document.createElement('p');
+  text.textContent = '0%';
+
+  let reportedProgress = false;
+  return [
+    wrap,
+    n => {
+      if (!reportedProgress) {
+        wrap.append(text);
+        progress.classList.remove('loader');
+        progress.classList.add('progress');
+      }
+      n = Math.max(0, Math.min(1, n)) * 100;
+      progress.style.setProperty('--progress', `${n}%`);
+      text.textContent = `${n.toFixed(0)}%`;
+    },
+  ];
+}
+
+async function uploadFile(file) {
+  const [content, progressCb] = createUploading();
+  const overlay = createOverlay({ title: 'Uploading...', content });
+  try {
+    const link = await upload(file, progressCb);
+    overlay.update({ title: 'Done', content: createSuccess(link, () => overlay.remove()) });
+  } catch (e) {
+    overlay.update({ title: 'Error', content: createError(e.toString(), () => overlay.remove()) });
+  }
+}
+
+function upload(file, progressCb) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('progress', ({ loaded, total }) => {
+      progressCb(loaded / total);
+    });
+    xhr.addEventListener('load', () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        try {
+          const { link } = JSON.parse(xhr.responseText);
+          if (!link) {
+            throw new Error('No link in response');
+          }
+          resolve(link);
+        } catch (e) {
+          reject(e);
+        }
+      } else {
+        reject(tryJsonErrorXhr(xhr));
+      }
+    });
+    xhr.addEventListener('error', () => {
+      reject(new Error('Request failed'));
+    });
+    xhr.addEventListener('abort', () => {
+      reject(new Error('Aborted'));
+    });
+    xhr.open('POST', '/upload');
+    xhr.setRequestHeader('Authorization', localStorage.getItem(LOCAL_STORAGE_KEY));
+    xhr.send(file);
+  });
+}
+
+function tryJsonErrorXhr(xhr) {
+  const json = xhr.getResponseHeader('content-type').startsWith('application/json')
+    ? JSON.parse(xhr.responseText)
+    : xhr.responseText;
+  return new Error(
+    `${xhr.status} ${xhr.statusText} - ${
+      typeof json === 'string' ? json : json?.message ?? json?.error ?? JSON.stringify(json)
+    }`,
+  );
+}
