8741c23ad59563bdacd8e2fd7307fb5db10a5ef9
Nerixyz <nerixdev@outlook.de>
2025-01-27 16:40:02 +0100

    fix: rng

--- src/rng.rs
+++ src/rng.rs
@@ -1,14 +1,14 @@
-use rand::{distributions::Distribution, thread_rng, Rng};
+use rand::{rng, Rng};
 
 pub fn generate_name() -> String {
-    let rng = thread_rng();
+    let rng = rng();
     let name = rng.sample_iter(Filename).take(7).collect();
     String::from_utf8(name).expect("This will always succeed since `Filename` only emits ASCII characters which are valid UTF-8.")
 }
 
 struct Filename;
 
-impl Distribution<u8> for Filename {
+impl rand::distr::Distribution<u8> for Filename {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> u8 {
         const GEN_ASCII_STR_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                 abcdefghijklmnopqrstuvwxyz\
